8486150092743a705bd0bd2a53c78c749f6128d1
Montserrat Gomez <montserrat.ga95@gmail.com>
2024-07-02 15:02:48 -0600

    Initial Commit

---
+++ SocialMedia/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SocialMedia
+//
+//  Created by Montserrat Gomez on 02/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SocialMedia/SocialMediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialMediaApp.swift
+//  SocialMedia
+//
+//  Created by Montserrat Gomez on 02/07/24.
+//
+
+import SwiftUI
+
+@main
+struct SocialMediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
